10c90824b0f611088c758dfe4f2987624b4fddae
juanjosecaballero08 <juanjosecaballero08@iesamachado.org>
2026-04-06 09:45:27 +0200

    mario gira

--- js/move.js
+++ js/move.js
@@ -5,9 +5,11 @@ document.addEventListener("keydown", (event) => {
     switch (event.key) {
         case "ArrowLeft":
             posicionX = posicionX - 10;
+            mario.style.transform = "scaleX(-1)";
             break;
         case "ArrowRight":
             posicionX = posicionX + 10;
+            mario.style.transform = "scaleX(1)";
             break;
 
     }
